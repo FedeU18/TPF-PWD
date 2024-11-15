4af7a8f96bddd8c82628cd62404f62a07d03ad05
JoaquinIVL95 <joaquinIVl95@gmail.com>
2024-11-14 23:03:30 -0300

    Actualizacion en registro, asignacion y seteo de rol a nuevo usuario

--- Vista/registro/action.php
+++ Vista/registro/action.php
@@ -1,16 +1,14 @@
 <?php 
-// include_once "../../Utils/funciones.php";
+
 include_once "../../config.php";
 
 // Este archivo recibe los datos del formulario
-// Debe chequear que el usuario y el correo no esten registrados ya para otro usuario
-// Debe verificar que el correo sea válido
-// Debe verificar que el usuario tenga al menos 8 caracteres
+// Chequea que el usuario y el correo no esten registrados ya para otro usuario
+// Verifica que el correo sea válido
+// Verifica que el usuario tenga al menos 8 caracteres
 // Si todo esta bien realiza un hash de la contraseña para luego setear el nuevo usuario
-// Si todo esta bien, redirige a la página de inicio?
+// Si todo esta bien, redirige a la página de inicio
 // Si no todo esta bien, muestra un mensaje de error y redirige a la página de registro
-
-
 
 $datos = data_submitted();
 
@@ -19,20 +17,20 @@ $usuarios = $objUsuario->buscar(null);
 $encontrado = false;
 
 foreach($usuarios as $usuario){
-    if($usuario['usuario'] == $datos['usnombre']) {
+    if($usuario->getNombre() == $datos['usnombre']) {
         $mensaje = "El usuario ya existe";
         $encontrado = true;
-        header("Location: registro.php?mensaje=$mensaje");
+        header("Location: index.php?mensaje=$mensaje");
         exit;
     }
-    if($usuario['correo'] == $datos['email']) {
+    if($usuario->getCorreo() == $datos['email']) {
         $mensaje = "El correo ya existe";
         $encontrado = true;
-        header("Location: registro.php?mensaje=$mensaje");
+        header("Location: index.php?mensaje=$mensaje");
         exit;
     }
-
 }
+
 $err = 0;
 if($encontrado == false){
     if(filter_var($datos['email'], FILTER_VALIDATE_EMAIL)){
@@ -40,12 +38,42 @@ if($encontrado == false){
         if(strlen($datos['usnombre']) <= 8){
             $err = 2;
             $pass = password_hash($datos['pass'], PASSWORD_DEFAULT);
-            $objUsuario->alta(null, $datos['usnombre'], $pass ,$datos['email'], null);
-            header("Location: ../index.php");
+            $nuevoUsuario = [
+                "idusuario" => null,
+                "usnombre" => $datos['usnombre'],
+                "uspass" => $pass,
+                "usmail" => $datos['email'],
+                "usdeshabilitado" => null
+            ];
+            
+            // Registrar el usuario
+            $usuarioCreado = $objUsuario->alta($nuevoUsuario);
+
+            if ($usuarioCreado) {
+                // Asignar el rol al usuario recién creado
+                $objUsuarioRol = new ABMUsuarioRol;
+
+                // Obtiene el id del usuario recién creado
+                $idUsuarioCreado = $objUsuario->buscar(["usnombre" => $datos['usnombre']])[0]->getIdUsuario();
+
+                
+                $idRolCliente = 2; 
+
+                // Crear el registro en usuariorol
+                $nuevoUsuarioRol = [
+                    "idusuario" => $idUsuarioCreado,
+                    "idrol" => $idRolCliente
+                ];
+                
+                $objUsuarioRol->alta($nuevoUsuarioRol);
+            }
+            
+            $exito = header("Location: ../index.php");
             exit;
         }
     }
 }
 echo $err;
-
 ?>
+
+
