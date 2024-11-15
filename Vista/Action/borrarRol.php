badee4cb6f1eb2296b35fe08d2069ed333bf5754
emanuelPinedo <emanuelpinedo7@gmail.com>
2024-11-15 17:53:22 -0300

    borrar rol hecho

---
+++ Vista/Action/borrarRol.php
@@ -0,0 +1,19 @@
+<?php
+include_once("../../config.php");
+
+$datos = data_submitted();
+$abmUsuario = new ABMUsuario();
+$mensaje = "";
+
+if (isset($datos['idusuario']) && isset($datos['idrol'])) {
+    if ($abmUsuario->borrarRol($datos)) {
+        $mensaje = "Rol eliminado correctamente.";
+    } else {
+        $mensaje = "Error al eliminar el rol.";
+    }
+} else {
+    $mensaje = "Datos insuficientes para eliminar el rol.";
+}
+
+echo "<script>location.href='../listaUsers.php?msg=" . htmlspecialchars($mensaje) . "';</script>";
+?>
