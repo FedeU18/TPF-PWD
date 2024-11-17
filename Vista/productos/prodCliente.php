2b7c223a39837862621e3422c33c73c1b10eae4f
Federico <fedee.unates.2001@gmail.com>
2024-11-17 02:00:04 -0300

    vista de productos para admin y cliente

--- Vista/productos/prodCliente.php
+++ Vista/productos/prodCliente.php
@@ -1,1 +1,21 @@
-<h1>HOLA CLIENTITO</h1>
+<?php
+
+$objProductos = new ABMProducto();
+$productos = $objProductos->buscar(null);
+?>
+
+<h1 class="text-center mb-4">Listado de Productos</h1>
+<div class="row">
+  <?php foreach ($productos as $producto) { ?>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo htmlspecialchars($producto->getpronombre()); ?></h5>
+          <p class="card-text"><?php echo htmlspecialchars($producto->getprodetalle()); ?></p>
+          <p class="card-text"><strong>Stock:</strong> <?php echo htmlspecialchars($producto->getprocantstock()); ?></p>
+          <a class="btn btn-success">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
+</div>
