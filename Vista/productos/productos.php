794e8cfcc3200c2d788ae71fe30925b8b297f312
Federico <fedee.unates.2001@gmail.com>
2024-11-20 00:37:17 -0300

    lógica post pasarela de pagos, se crea el registro de compra, compraestado y compraitem

--- Vista/productos/productos.php
+++ Vista/productos/productos.php
@@ -4,10 +4,9 @@ $titulo = "productos";
 include_once "../../estructura/headerSeguro.php";
 $userRol = $session->getRol();
 echo "<h1>Productos</h1>";
-
-if ($userRol == 2) {
+if ($userRol[0] == 2) {
   include_once "prodAdmin.php";
-} elseif ($userRol == 1) {
+} elseif ($userRol[0] == 1) {
   include_once "prodCliente.php";
 }
 
